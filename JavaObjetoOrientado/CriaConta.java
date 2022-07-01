8b6cb3b71615dd99c587912e1b40a50b90c410e9
Heloisa Busquim <hbusquim@gmail.com>
2022-07-01 16:03:19 -0300

    lesson 4

--- JavaObjetoOrientado/CriaConta.java
+++ JavaObjetoOrientado/CriaConta.java
@@ -3,12 +3,5 @@ package JavaObjetoOrientado;
 public class CriaConta {
 
     public static void main(String[] args) {
-        Conta primeiraConta = new Conta();
-        Conta segundaConta = new Conta();
-        primeiraConta.deposita(200);
-        segundaConta.deposita(300);
-        primeiraConta.transfere(100, segundaConta);
-        System.out.println(primeiraConta.saldo);
-        System.out.println(segundaConta.saldo);
     }
 }
