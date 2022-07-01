722de76cfa83bcfd5752a8c360a12718e3c15ce0
Heloisa Busquim <hbusquim@gmail.com>
2022-07-01 15:18:01 -0300

    lesson 2

---
+++ JavaObjetoOrientado/CriaConta.java
@@ -0,0 +1,10 @@
+package JavaObjetoOrientado;
+
+public class CriaConta {
+
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 300;
+        System.out.println(primeiraConta.saldo);
+    }
+}
